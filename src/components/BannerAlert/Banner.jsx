d9fb138fc13fa7e2e89657795dd359746e3dd621
onava <enava23@yahoo.com>
2021-12-08 02:56:56 -0500

    title subtitle

--- src/components/BannerAlert/Banner.jsx
+++ src/components/BannerAlert/Banner.jsx
@@ -4,7 +4,7 @@ import "./style.scss";
 export default function Banner() {
   return (
     <div className="banner d-flex justify-content-center align-items-center">
-      <h6> Learn more about eGroups </h6>
+      <h6 id="banner-alert"> Learn more about eGroups </h6>
     </div>
   )
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,27 +1,39 @@
 import React from 'react';
 // import Cards from "./components/Cards";
 
+const titleStyle = {
+  fontFamily: 'Montserrat',
+  fontWeight: '700',
+  fontSize: '35px',
+  // letterSpacing: '.1rem',
+  marginTop: '5rem',
+}
+
+const subTitleStyle = {
+  fontFamily: 'Open Sans',
+  fontWeight: '300',
+  fontSize: '18px',
+  color: 'slategrey'
+  // letterSpacing: '.1rem',
+  // marginTop: '5rem',
+}
+
+const landingStyle = {
+  marginLeft: '1rem',
+  marginRight: '1rem',
+}
+
 export default function LandingPage() {
   return (
     <div>
-      <div>
-        <h3>Choose your community</h3>
-        <h6>Whether you are visiting a local campus or joining us online for the very first time, we cant wait to connect with you</h6>
-        <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
-            </p>
-            {/* <Cards /> */}
-      </div>
+      <div style={landingStyle}>
+        <span style={titleStyle} className="d-flex justify-content-center align-items-center text-center">
+          Choose your community
+        </span>
+        <span style={subTitleStyle} className="d-flex justify-content-center align-items-center text-center">
+          Whether you are visiting a local campus or joining us online for the very first time, we cant wait to connect with you
+        </span>
+      </div>    
     </div>
   )
 }
